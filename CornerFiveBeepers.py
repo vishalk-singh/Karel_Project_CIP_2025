c05aebbfe95500181ab65ede5df2bafebcf79f9c
vishalk-singh <charumitrasingh@gmail.com>
2025-06-21 07:25:01 +0530

    New Karel Exercise

--- CornerFiveBeepers.py
+++ CornerFiveBeepers.py
@@ -1,21 +1,29 @@
 # File: CornerFiveBeepers.py
 # -----------------------------
-# Pick five beepers in each corner
+# Pick five beepers in each corner.
+# End at bottom left corner facing east.
 
 from karel.stanfordkarel import *
 
 
 def main():
-   pass
-
-
-def turn_right():
-   for i in range(3):
-      turn_left()
-
-def turn_around():
+    while not facing_south():
+        collect_all_beepers()
+        move_to_wall()
+        turn_left()
+    collect_all_beepers()
+    move_to_wall()
     turn_left()
-    turn_left()
+
+def collect_all_beepers():
+    while beepers_present():
+        if beepers_present():
+            pick_beeper()
+
+
+def move_to_wall():
+    while front_is_clear():
+        move()
 
 
 if __name__ == "__main__":
