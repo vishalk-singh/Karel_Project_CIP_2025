84f588a5e90729aaf7266e77c736d68807536c34
vishalk-singh <charumitrasingh@gmail.com>
2025-06-21 07:38:04 +0530

    New Karel Exercise

--- ExtensionKarel.py
+++ ExtensionKarel.py
@@ -3,17 +3,12 @@ from karel.stanfordkarel import *
 """
 File: ExtensionKarel.py
 -----------------------
-This file is for optional extension programs. 
+This file is for optional extension programs. Write your code here.
 """
 
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
     pass
+
 
 
 # There is no need to edit code beyond this point
--- FillBottomRow.py
+++ FillBottomRow.py
@@ -6,11 +6,22 @@ File: MoveFiveKarel.py
 Karel completes a puzzle by:
 1. Karel move farward 5 times.
 2. Put Beepers in the bottom row.
+3. Then move till the end of the puzzle.
+4. Define a new function move_to_wall after putting 5 beepers for move till the end of the puzzle.
 """
 def main():
-    pass
+    put_five_beepers_in_row()
+    move_to_wall()
 
+def put_five_beepers_in_row():
+    for i in range(5):
+        if no_beepers_present():
+            put_beeper()
+        move()
 
+def move_to_wall():
+    while front_is_clear():
+        move()
 
 
 # There is no need to edit code beyond this point
